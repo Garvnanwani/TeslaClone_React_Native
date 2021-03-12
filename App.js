bb009d1b02f90182438a8a4a7c387c99d213f00a
Garvnanwani <garvnanwani88@gmail.com>
2021-03-12 23:27:26 +0530

    initial app

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar'
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.carContainer}>
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subtitle}>Starting at $70000</Text>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+
+  carContainer: {
+    width: '100%',
+    height: '100%',
+  },
+
+  titles: {
+    marginTop: '30%',
+    width: '100%',
+    alignItems: 'center',
+  },
+
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+
+  subtitle: {
+    fontSize: 16,
+    color: '#5c5e62',
+  },
+})

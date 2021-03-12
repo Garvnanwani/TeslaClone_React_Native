d44eb18752d74b06b5bc2965de66dd5ab536aa02
Garvnanwani <garvnanwani88@gmail.com>
2021-03-13 00:46:16 +0530

    button component added

--- components/Caritem/index.js
+++ components/Caritem/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {ImageBackground, Text, View} from 'react-native'
+import StyledButton from '../StyledButton'
 import styles from './styles'
 const CarItem = (props) => {
   return (
@@ -12,6 +13,8 @@ const CarItem = (props) => {
         <Text style={styles.title}>Model S</Text>
         <Text style={styles.subtitle}>Starting at $69,500</Text>
       </View>
+
+      <StyledButton />
     </View>
   )
 }
